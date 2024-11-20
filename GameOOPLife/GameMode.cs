7e97b0d6604de05d36373d05456536882ca814ee
trajil <trajil@web.de>
2024-11-20 15:16:14 +0100

    added GameMode class

---
+++ GameOOPLife/GameMode.cs
@@ -0,0 +1,22 @@
+﻿namespace GameOOPLife;
+
+public static class GameMode
+{
+    public static void RunMode(Overseer overseer, string mode = "random")
+    {
+        switch (mode)
+        {
+            case "random":
+                {
+                    overseer.ReviveSomeRandomCells();
+                }
+                break;
+
+            case "custom":
+                {
+
+                }
+                break;
+        }
+    }
+}
